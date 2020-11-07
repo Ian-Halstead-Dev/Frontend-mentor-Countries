069f3b5730907a1a9eadd60613d82ae82d3eb1c4
Ian Halstead <iwhalstead10@gmail.com>
2020-11-06 20:20:16 -0700

    deleted backend folder

--- backend/db/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-mongoose.connect(process.env.DBURL, {
-	useNewUrlParser: true,
-	useCreateIndex: true,
-	useFindAndModify: false,
-	useUnifiedTopology: true
-});
--- backend/routers/frontendRouter.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-const express = require('express');
-
-const router = express.Router();
-
-router.get('*', (req, res) => res.sendFile(path.join(__dirname, '../', '../', 'frontend', 'public', 'index.html')));
-
-module.exports = router;
